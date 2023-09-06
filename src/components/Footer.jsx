c5a0d1ec3cd5497aa401c6e4f90ff9b44a39e503
Sidak Singh Sra <105771804+sidaksra@users.noreply.github.com>
2023-09-06 14:54:10 +0530

    Created First React App

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Tech</h1>
+        <p>@all rights reserved</p>
+      </div>
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="" target={"blank"}>Portfolio</a>
+          <a href="" target={"blank"}>LinkedIn</a>
+          <a href="" target={"blank"}>GitHub</a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
